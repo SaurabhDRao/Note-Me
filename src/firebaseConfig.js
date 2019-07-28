e16eb93152adfb44c81c239952a13755ffcec6b0
KnightShade <=>
2019-07-28 12:58:26 +0530

    small changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDXr-jKwpZvDexhw5Kc9iABnsbFNfryAaE",
-    authDomain: "note-me-f6b4a.firebaseapp.com",
-    databaseURL: "https://note-me-f6b4a.firebaseio.com",
-    projectId: "note-me-f6b4a",
-    storageBucket: "note-me-f6b4a.appspot.com",
-    messagingSenderId: "48163254578",
-    appId: "1:48163254578:web:e696e2c57d03d128"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 export default firebaseConfig;
