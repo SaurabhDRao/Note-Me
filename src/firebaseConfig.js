55f27bae9526e4a5384a3588efbcd4acab8348a6
KnightShade <=>
2019-07-28 12:54:15 +0530

    minor changes

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXr-jKwpZvDexhw5Kc9iABnsbFNfryAaE",
+    authDomain: "note-me-f6b4a.firebaseapp.com",
+    databaseURL: "https://note-me-f6b4a.firebaseio.com",
+    projectId: "note-me-f6b4a",
+    storageBucket: "note-me-f6b4a.appspot.com",
+    messagingSenderId: "48163254578",
+    appId: "1:48163254578:web:e696e2c57d03d128"
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebaseConfig from "./firebaseConfig";
 
 import AuthContextProvider from "./components/contexts/AuthContext";
 
 const firebase = require("firebase");
 require("firebase/firestore");
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDXr-jKwpZvDexhw5Kc9iABnsbFNfryAaE",
-    authDomain: "note-me-f6b4a.firebaseapp.com",
-    databaseURL: "https://note-me-f6b4a.firebaseio.com",
-    projectId: "note-me-f6b4a",
-    storageBucket: "note-me-f6b4a.appspot.com",
-    messagingSenderId: "48163254578",
-    appId: "1:48163254578:web:e696e2c57d03d128"
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
